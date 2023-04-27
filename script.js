650825a63c6850fb7cb362ac3314837ba165a073
elizeujrdev <131943477+elizeujrdev@users.noreply.github.com>
2023-04-27 02:34:06 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function ativarletra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = ''
+    arrTexto.forEach((letra, i)=>{
+        setTimeout(()=> {
+
+            elemento.innerHTML += letra;
+        },75 * i)
+        
+    });
+
+
+
+}
+const titulo = document.querySelector('.digitando')
+ativarletra(titulo);
+
